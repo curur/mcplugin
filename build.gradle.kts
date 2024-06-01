589eaaf9f882797746b17b92e0002f943b3bd60a
curur <crurumain@gmail.com>
2024-06-01 23:25:30 +0900

    서버 생성

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,9 +1,16 @@
+import org.jetbrains.kotlin.com.google.gson.JsonParser
+import java.io.InputStreamReader
+import java.net.URL
+import java.nio.file.Files
+import java.nio.file.Path
+
 plugins {
     kotlin("jvm") version "1.8.0"
 }
 
 group = "com.github.curur"
 version = "0.0.1"
+val mcversion = properties["mcversion"]
 
 java {
     toolchain.languageVersion.set(JavaLanguageVersion.of(17))
@@ -15,18 +22,45 @@ repositories {
 }
 
 dependencies {
-//    compileOnly("io.papermc.paper:paper-api:1.19.4-R0.1-SNAPSHOT")
+//    compileOnly("io.papermc.paper:paper-api:$mcversion-R0.1-SNAPSHOT")
 }
 
 tasks {
     build {
         doLast {
+            val serverFolder = File("D:/server/dev server/$mcversion")
+            if (!serverFolder.exists()) {
+                serverFolder.mkdir()
+
+                // paper.jar
+                val url = URL("https://api.papermc.io/v2/projects/paper/versions/$mcversion")
+                val inputReader = InputStreamReader(url.openStream())
+                val builds = JsonParser.parseReader(inputReader).asJsonObject.get("builds").asJsonArray
+                val latestBuild = builds.get(builds.size() - 1).asString //제일 마지막 빌드
+
+                val downloadUrl = URL(
+                    "https://api.papermc.io/v2/projects/paper/versions/" +
+                            "$mcversion/builds/$latestBuild/downloads/paper-$mcversion-$latestBuild.jar"
+                )
+
+                val path = Path.of(File("$serverFolder/paper.jar").toURI())
+                downloadUrl.openStream().use {
+                    Files.copy(it, path)
+                }
+
+                inputReader.close()
+
+                //start.bat
+                File(serverFolder, "start.bat")
+                    .writeText("java -Xms128M -Xmx2G -jar paper.jar nogui")
+
+            }
             copy {
                 val jarName = "${rootProject.name}-$version.jar"
                 from("build\\libs\\$jarName")
-                into("D:\\server\\dev server\\plugins")
+                into("$serverFolder\\plugins")
             }
-            delete("D:\\server\\dev server\\plugins\\update\\RELOAD")
+            delete("$serverFolder\\plugins\\update\\RELOAD")
         }
     }
 }
